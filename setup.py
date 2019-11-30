d61957888ffc89a693dd868bcfa11434eda5c0ef
Lorenz <lorenz.hufe@web.de>
2019-11-30 16:20:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(name="gym_snake",
       version="0.1",
-      #url="https://github.com/tuzzer/gym-maze",
       author="Lorenz Hufe",
       license="MIT",
       packages=["gym_snake", "gym_snake.envs"],
